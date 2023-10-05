0e2cb396a2909861e4b0af4b68b94e301cf971ff
Zain Jerath <zainjerath@gmail.com>
2023-10-04 19:06:03 -0500

    added banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import RestroomCard from "./components/Card/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "./components/Banner/Banner";
+import BathroomHeader from "./components/BathroomHeader/BathroomHeader";
 
 const App = () => {
   return (
     <div className="App">
+      <Banner />
+      <BathroomHeader />
       <div className="restroom-cards">
         <RestroomCard name={"Chipotle"} address={"123 W Jackson Rd"} distance={"372 ft"} busy={"busier than usual"} rating={"5"}/>
         <RestroomCard name={"Chipotle"} address={"123 W Jackson Rd"} distance={"372 ft"} busy={"busier than usual"} rating={"5"}/>
