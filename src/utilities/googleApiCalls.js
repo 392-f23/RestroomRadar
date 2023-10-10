6306ca5e95944e113fbc51e033266f3ed565561a
maseh90 <MasonHartweger2023@u.northwestern.edu>
2023-10-10 14:41:32 -0500

    updated api return field access

--- src/utilities/googleApiCalls.js
+++ src/utilities/googleApiCalls.js
@@ -7,16 +7,18 @@ export const getCoordinateLocation = (queryLocation,setCoordinates) => {
     // query structure: https://developers.google.com/maps/documentation/javascript/places#find_place_from_query
     var requestLocation = {
       query: queryLocation,
-      fields: ['name', 'geometry']
+      fields: ['name', 'geometry','geometry.location']
     };
       
     // iterating through places from API request: https://developers.google.com/maps/documentation/javascript/places#find_place_from_query
     placesService.findPlaceFromQuery(requestLocation, function(results, status) {
       if (status === window.google.maps.places.PlacesServiceStatus.OK) {
         for (var i = 0; i < results.length; i++) {
-          let lon = results[i].geometry.viewport.Ta.hi;
-          let lat = results[i].geometry.viewport.rb.hi;
+          console.log(results[i]);
+          let lon = results[i].geometry.viewport.Oa.hi;
+          let lat = results[i].geometry.viewport.mb.hi;
           setCoordinates({lat, lon});
+          //console.log({lat,lon});
         }
       }
     }
@@ -24,10 +26,12 @@ export const getCoordinateLocation = (queryLocation,setCoordinates) => {
 }
   
 export const getNearbyRestrooms = (queryCoordinates,setNearbyPlaces,setRestroomData) => {
-
+    //console.log("queryCoordinates");
+    //console.log(queryCoordinates);
     var currentLocation = new google.maps.LatLng(queryCoordinates.lat,queryCoordinates.lon);
     var placesService = new google.maps.places.PlacesService(document.createElement("div"));
-
+    //console.log("currentLocation");
+    //console.log(currentLocation);
     // query structure: https://developers.google.com/maps/documentation/javascript/places#place_search_requests
     var requestNearby = {
       location: currentLocation,
@@ -39,14 +43,17 @@ export const getNearbyRestrooms = (queryCoordinates,setNearbyPlaces,setRestroomD
     placesService.nearbySearch(requestNearby, function(results, status) {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         let nearbyResults = [];
+        //console.log("got here too");
+        //console.log(results);
         for (var i = 1; i < results.length; i++) {
+        //console.log(results[i]);
           nearbyResults.push({
             "id": results[i].place_id, 
             "name": results[i].name,
             "address": results[i].vicinity,
             "distance": getDistanceFromLatLonInKm(
-              results[i].geometry.viewport.rb.hi,
-              results[i].geometry.viewport.Ta.hi,
+              results[i].geometry.viewport.mb.hi,
+              results[i].geometry.viewport.Oa.hi,
               queryCoordinates.lat,
               queryCoordinates.lon).toFixed(2),
             "rating": results[i].rating,
@@ -55,7 +62,7 @@ export const getNearbyRestrooms = (queryCoordinates,setNearbyPlaces,setRestroomD
           });}
         setNearbyPlaces(nearbyResults);
         setRestroomData(nearbyResults);
-        console.log(nearbyResults);
+        //console.log(nearbyResults);
       }
     });
 }
