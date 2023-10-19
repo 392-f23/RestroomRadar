1115dc2c2be6bfc4035c59a33c9fae7c58de7882
Zain Jerath <zainjerath@gmail.com>
2023-10-18 22:37:25 -0500

    review styling

--- src/components/ReviewCard.jsx/ReviewCard.jsx
+++ src/components/ReviewCard.jsx/ReviewCard.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import { Button, Stack } from "react-bootstrap";
+import { Button, Stack, Card } from "react-bootstrap";
 import StarRating from "../StarRating/StarRating";
 import "./ReviewCard.css";
 
 export const ReviewCard = ({ username, review, rating, photo }) => {
   return (
-    <Stack direction="horizontal" className="p-1 ">
-      {photo ? (
-        <img className="review_pic" src={photo} width="50" height="50"></img>
-      ) : (
-        <div className="photo_placeholder"></div>
-      )}
-
-      <Stack direction="vertical" className="p-2">
-        <div className="review_username">{username}</div>
-        <div className="review_text">{review}</div>
-        <div className="review_rating">
-          <StarRating rating={rating} />
+    <Card className="mb-3 p-1">
+      <Stack direction="vertical" className="p-1 reviewcard">
+        <div className="d-flex align-items-center">
+          {photo ? (
+            <img className="review_pic" src={photo} width="30" height="30"></img>
+          ) : (
+            <div className="photo_placeholder"></div>
+          )}
+          <div className="review_username">{username}</div>
         </div>
+
+        <Stack direction="vertical">
+          <div className="review_rating">
+            <StarRating rating={rating} />
+          </div>
+          <div className="review_text">{review}</div>
+        </Stack>
       </Stack>
-    </Stack>
+    </Card>
   );
 };
